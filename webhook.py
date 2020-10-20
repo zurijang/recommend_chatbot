3075a6a8144226fe84ca29ef13383253567f5048
zurijang <wkdwn05@gmail.com>
2020-10-20 23:06:47 +0900

    update

--- webhook.py
+++ webhook.py
@@ -4,4 +4,4 @@ from telegram_bot import settings
 bot = telegram.Bot(token=settings.TELEGRAM_TOKEN)
 
 # ngrok http 8000
-bot.set_webhook("https://1340c36176db.ngrok.io/chatbot/Service/")
+bot.set_webhook("https://407a7d0b426b.ngrok.io/chatbot/Service/")
