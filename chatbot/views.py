8a3b4b6964efa0bf3b34c0501546979ad480ac80
zurijang <wkdwn05@gmail.com>
2020-10-22 02:39:18 +0900

    DB값사용에서 CSV 파일로 결과값나오도록 변경

--- chatbot/views.py
+++ chatbot/views.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from .tasks import pearsonR, recommend, input_validation, extract_text
 import telegram
 import json
+import pandas as pd
 # Create your views here.
 
 bot = telegram.Bot(token=settings.TELEGRAM_TOKEN)
@@ -27,7 +28,9 @@ def Service(request):
 
     else:
         recommend_list = recommend(received_text)
-        text = extract_text(recommend_list)
+        df_recommend_result = pd.DataFrame(recommend_list, columns = ['상세 검색지명', '상관계수', '유형']).values
+        recommend_df = pd.DataFrame(df_recommend_result, columns = ['상세 검색지명', '상관계수', '유형'])[:5]
+        text = extract_text(recommend_df)
 
     bot.sendMessage(chat_id=chat_id, text=text)
     return HttpResponse('Success')
